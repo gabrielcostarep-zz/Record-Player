c477520e3be41d22e1bcb5b40408271cf2cd1361
Gabriel Costa <gabrielcosta12377@hotmail.com>
2021-07-31 02:00:28 -0300

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -7,21 +7,27 @@ var rotacaoAgulha;
 let rot = 0;
 let ang = 0;
 
+// Iniciar a musica com um volume baixo
 music.volume = 0.1;
 
+// Funcao para iniciar a musica
 function play() {
     if (ang == 0) {
         ang = 16;
     }
 
+    // Altera o angulo da agulha antes da musica iniciar
     agulha.style = "transform: rotate(" + ang + "deg); right: 9%;";
 
     function touch() {
         music.play();
+
+        // Funcao responsavel pela agulha avancar conforme a musica for tocando
         rotacaoAgulha = setInterval(function () {
             ang += 0.5;
             agulha.style = "transform: rotate(" + ang + "deg); right: 9%;";
 
+            // Caso a musica acabe a funcao stop é chamada
             if (music.ended == true) {
                 setTimeout(stop, 0);
             }
@@ -30,12 +36,14 @@ function play() {
 
     touch();
 
+    // Funcao responsavel por fazer o vinil girar
     rotacaoVinil = setInterval(function () {
         rot++
         vinil.style = "transform: rotate(" + rot + "deg)";
     }, 6);
 }
 
+// Funcao para pausar a musica
 function pause() {
     music.pause();
     rot += 10;
@@ -45,6 +53,7 @@ function pause() {
     clearInterval(rotacaoAgulha);
 }
 
+// Funcao para parar a musica
 function stop() {
     music.pause()
     music.currentTime = 0;
@@ -58,6 +67,7 @@ function stop() {
     clearInterval(rotacaoAgulha);
 }
 
+// Funcao para alterar o volume da musica
 function volume(value) {
     music.volume = value / 10;
 }
